1a98ff402176d1fab47ca12403f25484da7d11e8
RAMON EMMIEL JASMIN <ramonjasmin8@gmail.com>
2023-01-27 09:42:39 +0800

    first commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int[] arrayInt = {1, 2, 3, 4, 5};
+
+        String[] arrayString = {"Allen", "Benny", "Cole", "Dexter", "Eman"};
+
+    }
+}
---
+++ src/SearchAlgorithms.java
@@ -0,0 +1,63 @@
+public class SearchAlgorithms {
+
+    public static boolean linearSearch(int key, int[] array){
+        for (int i = 0; i < array.length; i++){
+            if (key == array[i]){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean linearSearch(String key, String[] array){
+
+        for (int i = 0; i < array.length; i++){
+            if(key.equals(array[i])){
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public static boolean binarySearch(int key, int[] array){
+
+        int lowerIndex = 0;
+        int upperIndex = array.length - 1;
+        int midIndex = (lowerIndex + upperIndex) / 2;
+
+        while(lowerIndex <= upperIndex){
+            midIndex = (lowerIndex + upperIndex) / 2;
+
+            if (key > array[midIndex]){
+                lowerIndex = midIndex + 1;
+            } else if (key < array[midIndex]) {
+                upperIndex = midIndex - 1;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean binarySearch(String key, String[] array){
+
+        int lowerIndex = 0;
+        int upperIndex = array.length - 1;
+        int midIndex = (lowerIndex + upperIndex) / 2;
+
+        while(lowerIndex <= upperIndex){
+            midIndex = (lowerIndex + upperIndex) / 2;
+
+            if(key.compareTo(array[midIndex]) >= 1){
+                lowerIndex = midIndex + 1;
+            } else if (key.compareTo(array[midIndex]) <= -1) {
+                upperIndex = midIndex - 1;
+            } else {
+                return true;
+            }
+
+        }
+        return false;
+    }
+}
